b8f635647aca5eba425077beac5eb711245521a1
shumakovd <shumakov_d@hotmail.com>
2022-08-22 10:39:56 +0300

    Initial Commit

---
+++ Project-E/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-E
+//
+//  Created by Shumakov Dmytro on 22.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
